8e2d6a15d0568866e8aa4be43d758f51236c4e05
ShaginIlia <166448106+ShaginIlia@users.noreply.github.com>
2024-07-22 17:23:33 +0300

    Add files via upload

--- HW36.py
+++ HW36.py
@@ -7,46 +7,50 @@ class Table:
     def __init__(self, number, is_busy=False):
         self.number = int(number)
         self.is_busy = is_busy
+        self.lock = threading.Lock()
 
 
 class Customer(threading.Thread):
-    def __init__(self, customer_count):
+    def __init__(self, customer_count, table_number):
         threading.Thread.__init__(self)
         self.customer_count = customer_count
+        self.table_number = table_number
 
     def run(self):
-        print('Гость на обслуживании')
+        with self.table.lock:
+            print(f'Гость {self.customer_count} сел за столик номер {self.table_number}')
+            time.sleep(5)
+            print(f'Гость {self.customer_count} ушёл')
+            self.table.is_busy = False
 
 
 class Kafe:
-
-
     def __init__(self, tables):
         self.queue = Queue()
         self.tables = tables
 
-
     def customer_arrival(self):
         customer_count = 0
         while True:
-            cust = Customer(customer_count)
             customer_count += 1
-            self.queue.put(cust)
-            print(f'Гость {customer_count} прибыл. ')
-            time.sleep(1)
+            self.queue.put(customer_count)
+            print(f'Гость {customer_count} прибыл и добавлен в очередь')
+            time.sleep(2)
 
-    def serve_customer(self, cust):
-        for table in self.tables:
-            if not table.is_busy:
-                print(f'Гость садиться за свободный столик номер')
-                table.is_busy = True
-                cust.start()
-                time.sleep(5)
-                table.is_busy = False
-                print(f'Гость ушёл')
-                break
-            else:
-                print('Все столики заняты. Гость в очереди')
+    def serve_customer(self):
+        while True:
+            if not self.queue.empty():
+                customer_count = self.queue.get()
+                for table in self.tables:
+                    if not table.is_busy:
+                        table.is_busy = True
+                        cust = Customer(customer_count, table.number)
+                        cust.table = table
+                        cust.start()
+                        break
+                else:
+                    print(f'Все столики заняты. Гость {customer_count} в очереди')
+            time.sleep(1)
 
 
 table1 = Table(1)
@@ -59,13 +63,8 @@ cafe = Kafe(tables)
 customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
 customer_arrival_thread.start()
 
-while True:
-    if not cafe.queue.empty():
-        customer = cafe.queue.get()
-        cafe.serve_customer(customer)
-
-
-
+serve_customer_thread = threading.Thread(target=cafe.serve_customer)
+serve_customer_thread.start()
 
 
 
